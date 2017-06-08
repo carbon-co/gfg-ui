b6f71d38d43c7ba635aa8c59b7ace6e72b35391b
Ben Fu <ben_fu1@yahoo.com>
2017-06-08 11:40:03 -0400

    Fix server

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const Cors = require("kcors");
 const app = new Koa();
 
 // $ GET /package.json
+
 app.use(Cors());
-app.use(serve('.'));
+
+app.use(serve(__dirname));
+app.use(serve(`${__dirname}/html`));
+app.use(serve(`${__dirname}/css`));
+app.use(serve(`${__dirname}/images`));
+app.use(serve(`${__dirname}/json`));
 
 app.listen(8000);
 
